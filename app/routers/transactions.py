7bace4dc1b076cf97fc1b9718ea90756149e4f3b
Irawan Rofi Hidayat <irawan.rofi.ft21@mail.umy.ac.id>
2026-05-07 10:55:07 +0700

    menambahkan fitur edit

--- app/routers/transactions.py
+++ app/routers/transactions.py
@@ -41,6 +41,32 @@ async def read_transactions(
     transactions = result.scalars().all()
     return transactions
 
+# Endpoint buat update data (Edit)
+@router.put("/{transaction_id}", response_model=TransactionResponse)
+async def update_transaction(
+    transaction_id: int,
+    transaction_update: TransactionCreate,
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+) :
+    # 1. Cari datanya dulu
+    result = await db.execute(
+        select(Transaction).where(Transaction.id == transaction_id, Transaction.user_id == current_user.id)
+    )
+    db_transaction = result.scalar_one_or_none()
+    
+    if not db_transaction:
+        raise HTTPException(status_code=404, detail="Catatan tidak ditemukan")
+    
+    # 2. Update field yang dikirim dari frontend
+    update_data = transaction_update.model_dump()
+    for key, value in update_data.items():
+        setattr(db_transaction, key, value)
+        
+    await db.commit()
+    await db.refresh(db_transaction)
+    return db_transaction
+
 # Endpoint hapus transaksi
 @router.delete("/{transaction_id}")
 async def delete_transaction(
